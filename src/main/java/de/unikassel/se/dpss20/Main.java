792f23825e320acba419d1e313a82545c74e10bf
Adrian Kunz <clashsoft@hotmail.com>
2020-05-23 17:07:19 +0200

    Add Main and Launcher

---
+++ src/main/java/de/unikassel/se/dpss20/Main.java
@@ -0,0 +1,20 @@
+package de.unikassel.se.dpss20;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+// Used to start the app via Gradle (e.g. "gradle run").
+// DO NOT use this to start via IDE (it will error). Use the Launcher class instead.
+public class Main extends Application
+{
+	public static void main(String[] args)
+	{
+		Application.launch(Main.class, args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+
+	}
+}
